ebcb1c409e2a638b3667461b589d69b9d52d7b33
Ben Oliver <benjamin.e.oliver@gmail.com>
2022-10-21 19:11:53 +0200

    cleanup

--- jump.sh
+++ jump.sh
@@ -1,33 +1,23 @@
 #!/bin/sh
 
-set -eu
+set -o nounset
+set -o errexit
+
+_path_to_script="$(cd "$(dirname "$0")" || exit; pwd -P)/$(basename "$0")"
 
 bold=$(tput bold)
 normal=$(tput sgr0)
 underline=$(tput smul)
 nounderline=$(tput rmul)
 
-help_str="j(ump) around your filesystem
+help_str="j(ump) around your filesystem with tab completion...
 
 ${bold}SYNOPSIS${normal}
 
-    List all stored aliases and their paths:
-    j
-
-    Jump to the directory associated with NAME:
-    j NAME
-
-    Add a new alias for the current working directory:
-    j -a [NAME]
-
-    Delete a saved alias NAME:
-    j NAME -d
-
-    Rename a saved alias NAME to NEW_NAME:
-    j NAME -r NEW_NAME
+    j [NAME] [OPTION] ...
 
 ${bold}OPTIONS${normal}
-    -a (--add) [${underline}NAME${nounderline}]
+    -a (--add)
         Associate the ${underline}NAME${nounderline} with the current working directory. 
         If no ${underline}NAME${nounderline} is provided then the name of the current 
         working directory is used.
@@ -35,22 +25,41 @@ ${bold}OPTIONS${normal}
         Delete ${underline}NAME${nounderline} and any associated PATH.
     -r (--rename) ${underline}NEW_NAME${nounderline}
         Rename ${underline}NAME${nounderline} to ${underline}NEW_NAME${nounderline}.
-    -l (--list-names)
-        List all stored alias names.
+    -l (--list)
+        List all stored aliases and their paths.
     -g (--generated)
         Display the generated functions for your .bash_profile.
     -p (--purge)
         Remove any alias for a path that is not a directory.
     -h (--help)
         Display this help message and exit.
+
+${bold}EXAMPLES${normal}
+
+    List all stored aliases:
+    j
+
+    Jump to the directory associated with ${underline}NAME${nounderline}:
+    j ${underline}NAME${nounderline}
+
+    Create an alias for the current working directory using its basename:
+    j -a
+
+    Associate ${underline}NAME${nounderline} with the current working directory:
+    j ${underline}NAME${nounderline} -a
+
+    Delete a saved alias ${underline}NAME${nounderline}:
+    j ${underline}NAME${nounderline} -d
+
+    Rename a saved alias ${underline}NAME${nounderline} to ${underline}NEW_NAME${nounderline}:
+    j ${underline}NAME${nounderline} -r ${underline}NEW_NAME${nounderline}
+
 "
 
 help_and_exit () {
   echo "$help_str"
   exit
 }
-
-_path_to_script="$(cd "$(dirname "$0")" || exit; pwd -P)/$(basename "$0")"
 
 j_command_str="# The following function can be pasted into your shell profile.
 # calling the 'j' command with a single alias name is equivalent to calling
@@ -70,7 +79,7 @@ _jump_completions() {
   COMPREPLY=()
   local word=\"\${COMP_WORDS[COMP_CWORD]}\"
   if [ \"\$COMP_CWORD\" -eq 1 ]; then
-    COMPREPLY=(\$(compgen -W \"\$($_path_to_script --list-names)\" -- \"\$word\"))
+    COMPREPLY=(\$(compgen -W \"\$($_path_to_script)\" -- \"\$word\"))
   fi
 }
 
@@ -81,17 +90,17 @@ complete -F _jump_completions j
 # Entry point 
 # -----------------------------------------------------------------------------
 
-JUMP_CONFIG_DIR="$HOME/.config/jump"
-DB="$JUMP_CONFIG_DIR/jump.db"
+jump_config_dir="$HOME/.config/jump"
+jump_db="${jump_config_dir}/jump.db"
 
 # -----------------------------------------------------------------------------
 
-if [ ! -d "$JUMP_CONFIG_DIR" ]; then
-  echo "Create '$JUMP_CONFIG_DIR' [y/N]"
+if [ ! -d "${jump_config_dir}" ]; then
+  echo "Create '${jump_config_dir}' [y/N]"
   read -r confirmation
   if [ "$confirmation" = "y" ]; then
-    mkdir -p "$JUMP_CONFIG_DIR"
-    sqlite3 -line "${DB}" \
+    mkdir -p "${jump_config_dir}"
+    sqlite3 -line "${jump_db}" \
       "CREATE TABLE aliases(name TEXT NOT NULL PRIMARY KEY, path TEXT NOT NULL);"
     echo "$j_command_str"
   fi
@@ -101,58 +110,60 @@ fi
 # -----------------------------------------------------------------------------
 
 if [ ${#} -eq 0 ]; then
-  # input has length 0
-  # this indiecates that it is a `jump` command.
-  # we are friendly and list all stored alias/path pairs
-  sqlite3 -column "${DB}" "SELECT * FROM aliases;"
+  # We intrepret no args as a `jump` command. Just display all aliases...
+  sqlite3 -column "${jump_db}" "SELECT name FROM aliases;"
   exit
 fi
 
 if case $1 in -*) false;; *) true;; esac; then
-  NAME=$1
+  # first argument did not begin with a '-' prefix
+  # set alias_name to the first argument
+  alias_name=$1
   shift
-  if [ ${#} -eq 0 ]; then 
-    DIR=$(sqlite3 -list "${DB}" "SELECT path FROM aliases WHERE name='${NAME}';")
-    if [ -n "${DIR}" ]; then
-      echo "${DIR}"
-    fi
-    exit
+fi
+
+if [ ${#} -eq 0 ]; then 
+  # if no more args are present then just find the path for the 'alias_name'
+  alias_path=$(sqlite3 -list "${jump_db}" "SELECT path FROM aliases WHERE name='${alias_name}';")
+  if [ -n "${alias_path}" ]; then
+    echo "${alias_path}"
   fi
+  exit
 fi
 
 case $1 in
   -a | --add)
-    DIR=$(pwd)
-    NAME=${2:-$(basename "$DIR")}  
-    sqlite3 "${DB}" "INSERT INTO aliases VALUES ('${NAME}', '${DIR}');"
-    echo "Added alias name '${NAME}' for path '${DIR}'"
+    curr_dir=$(pwd)
+    alias_name=${alias_name:-$(basename "$curr_dir")}  
+    sqlite3 "${jump_db}" "INSERT INTO aliases VALUES ('${alias_name}', '${curr_dir}');"
+    echo "Added alias name '${alias_name}' for path '${curr_dir}'"
     ;;
   -d | --delete)
-    if [ -z "$NAME" ]; then
+    if [ -z "${alias_name}" ]; then
       help_and_exit
     fi  
-    sqlite3 -column "${DB}" "DELETE FROM aliases WHERE name='${NAME}';"
+    sqlite3 -column "${jump_db}" "DELETE FROM aliases WHERE name='${alias_name}';"
     ;;
   -r | --rename)
-    if [ -z "$NAME" ]; then
+    if [ -z "${alias_path}" ]; then
       help_and_exit
     fi
-    NEW_NAME=$2
-    if [ -z "$NEW_NAME" ]; then
+    new_alias_name=$2
+    if [ -z "${new_alias_name}" ]; then
       help_and_exit
     fi
-    sqlite3 -column "${DB}" \
-      "UPDATE aliases SET name='${NEW_NAME}' WHERE name='${NAME}';"
+    sqlite3 -column "${jump_db}" \
+      "UPDATE aliases SET name='${new_alias_name}' WHERE name='${alias_name}';"
     ;;
   -p | --purge)
-    sqlite3 -column "${DB}" "SELECT path FROM aliases;" | while read -r path ; do
+    sqlite3 -column "${jump_db}" "SELECT path FROM aliases;" | while read -r path ; do
       if [ ! -d "$path" ]; then
-        sqlite3 "${DB}" "DELETE FROM aliases WHERE path='${path}';"
+        sqlite3 "${jump_db}" "DELETE FROM aliases WHERE path='${path}';"
       fi
     done
     ;;
-  -l | --list-names)
-    sqlite3 -column "${DB}" "SELECT name FROM aliases;"
+  -l | --list)
+      sqlite3 -column "${jump_db}" "SELECT * FROM aliases;"
     ;;
   -g | --generate)
     echo "$j_command_str"
